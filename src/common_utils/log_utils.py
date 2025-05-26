206cec6f73ba8687702b23d0af800a7afc0bf14a
yura <perminov1901@bk.ru>
2025-05-27 02:41:38 +0300

    major fixes

--- src/common_utils/log_utils.py
+++ src/common_utils/log_utils.py
@@ -1,6 +1,4 @@
 def glue_string_lists(log: list[list[str]], max_widths=None, max_line_length: int = 150) -> list[str]:
-    if max_widths is None:
-        max_widths = []
     split_log = []
     for i in range(len(log)):
         stage = log[i]
